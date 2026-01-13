341ee24d18d342525e157fd90b510e65f2c05ad3
cybercodemomo <cybercodemomo@gmail.com>
2026-01-13 10:24:21 +0100

    placed all jsx files into components folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 // import { useState } from 'react'
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Login from './Authentication/Login/login'
-import Register from './Authentication/Registration/register'
-import FormBuilder from './App/main_app'
-import LandingPage from './Landing/Landingpage'
+// import Login from './Authentication/Login/login'
+// import Register from './Authentication/Registration/register'
+// import FormBuilder from './App/main_app'
+// import LandingPage from './Landing/Landingpage'
+
+import Login from './components/login'
+import Register from './components/register'
+import FormBuilder from './components/main_app'
+import LandingPage from './components/Landingpage'
 
 function App() {
 
