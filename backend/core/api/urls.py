d850405fe34e03fcb332f4fe42980ba6414153d5
cybercodemomo <cybercodemomo@gmail.com>
2026-01-16 10:04:38 +0100

    Feat: added permission for improved user expirenence, add serializers for data type convertions, fixed the google Oauth error and added buiness logic via viewset for general operations

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -1,17 +1,29 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-# from .views import FormViewSet, PublicFormViewSet, ResponseViewSet. 
+from .views import FormViewSet, ResponseViewSet, OptionViewSet, AnswerViewSet,QuestionViewSet
 from .views import SignupView, EmailLoginView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .views import GoogleAuthView
+# urlpatterns = [
+#     path('login/user', EmailLoginView.as_view(), name='login' ),
+#     path('register/user', SignupView.as_view(), name='register'),
+#     path("refresh/", TokenObtainPairView.as_view(), name="token_refresh"),
+#     path("auth/google/", GoogleAuthView.as_view()),
+
+# ]
+
+router = DefaultRouter()
+router.register(r'forms', FormViewSet) 
+router.register(r'questions', QuestionViewSet) 
+router.register(r'options', OptionViewSet) 
+router.register(r'responses', ResponseViewSet) 
+router.register(r'answers', AnswerViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('login/user', EmailLoginView.as_view(), name='login' ),
     path('register/user', SignupView.as_view(), name='register'),
     path("refresh/", TokenObtainPairView.as_view(), name="token_refresh"),
     path("auth/google/", GoogleAuthView.as_view()),
-
 ]
-# router.register("forms", FormViewSet, basename="forms")
-# router.register("public/forms", PublicFormViewSet, basename="public-forms")
-# router.register("responses", ResponseViewSet, basename="responses")
