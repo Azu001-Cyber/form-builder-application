1dd79b486a5d5b9e196b1abfffb330f9cb5ca8ae
cybercodemomo <cybercodemomo@gmail.com>
2026-01-17 10:12:59 +0100

    Fix: CORSheaders listing

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -149,6 +149,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5173",
     "https://formly-rho.vercel.app/",
 ]
