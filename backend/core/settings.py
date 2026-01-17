c480bfe625a5432a05183a75d5a553e30090172c
cybercodemomo <cybercodemomo@gmail.com>
2026-01-17 15:10:13 +0100

    Update: Settings Allowed Host II

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,11 +29,11 @@ DEBUG = True
 
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://formly-j875.onrender.com']
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME, 'https://formly-j875.onrender.com')
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default=SECRET_KEY)
 
