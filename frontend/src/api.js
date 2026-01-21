17a732a8972e1bc29f1fd451c27a0ca062e2a26a
cybercodemomo <cybercodemomo@gmail.com>
2026-01-21 14:31:16 +0100

    Update: back to deployment using render url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://formly-j875.onrender.com",
     });
 
     // Attach access token automatically
